b6cf7829a8ac2705722f3337bc5ebc32ae32e35e
digital opium <test@ismail.com>
2023-07-24 12:57:27 +0100

    set up excel import

---
+++ app/Imports/UsersImport.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\User;
+use Maatwebsite\Excel\Concerns\ToModel;
+
+class UsersImport implements ToModel
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new User([
+            'name' => $row['name'],
+            'email' => $row['email'],
+            'password' => $row['password']
+        ]);
+    }
+}
